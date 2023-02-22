6e13f51d462da391f523e1d3415cf16b802d968e
unknown <deveshsen512@gmail.com>
2023-02-22 11:34:33 -0500

    new

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -73,7 +73,7 @@ function Footer() {
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.0169960447724!2d77.07347761404787!3d28.689138188269826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d050158c062af%3A0x2a96869fbd0e4df9!2sA%20KUMAR%20HYDRAULIC%20HUB!5e0!3m2!1sen!2sin!4v1676614677692!5m2!1sen!2sin"
                 width="350"
                 height="250"
-                className="w-[80%] md:w-[80%] m-auto"
+                className="w-[80%]  m-auto"
                 allowFullScreen={true}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
